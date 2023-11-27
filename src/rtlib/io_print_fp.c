c0912ea54bf3735f9f130b2c71f5c0f55c8d720c
srvaldez <32546273+srvaldez@users.noreply.github.com>
2023-11-27 06:52:41 -0600

    Update io_print_fp.c

--- src/rtlib/io_print_fp.c
+++ src/rtlib/io_print_fp.c
@@ -10,6 +10,6 @@ FBCALL void fb_PrintSingle( int fnum, float val, int mask )
 
 FBCALL void fb_PrintDouble( int fnum, double val, int mask )
 {
-	char buffer[16+1+9+1];
-	fb_PrintFixString( fnum, fb_hFloat2Str( val, buffer, 16, FB_F2A_ADDBLANK ), mask );
+	char buffer[15+1+9+1];
+	fb_PrintFixString( fnum, fb_hFloat2Str( val, buffer, 15, FB_F2A_ADDBLANK ), mask );
 }
